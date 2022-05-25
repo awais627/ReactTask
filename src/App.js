7e62b0962f38ba1297bf84ecd39caf333b64a541
talk2awais <muhammad.awais@southvillesol.com>
2022-05-25 12:19:31 +0500

    downgrade React Dom v 17

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Home />
     </div>
   );
 }
